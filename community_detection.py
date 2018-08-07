b09d4aeb02d9da1778bfd80162a97f7e2c957796
amraw <amitrawat10@gmail.com>
2018-08-07 14:05:06 -0500

    test

--- community_detection.py
+++ community_detection.py
@@ -22,10 +22,18 @@ def community_detection():
     # Step 1
     step_1 = PrimitiveStep(primitive_description=d3m.primitives.sri.psl.CommunityDetection.metadata.query())
     step_1.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.0.produce')
-    add_hyperparameters(step_1, d3m.primitives.sri.psl.CommunityDetection)
+    step_1.add_hyperparameter(name='jvm_memory', argument_type=ArgumentType.VALUE, data=0.5)
     step_1.add_output('produce')
     pipeline_description.add_step(step_1)
 
-    pipeline_description.add_output(name='Result', data_reference='steps.1.produce')
+    # Step 2: RemoveColumns
+    step_2 = PrimitiveStep(primitive_description=d3m.primitives.data.RemoveColumns.metadata.query())
+    step_2.add_argument(name='inputs', argument_type=ArgumentType.CONTAINER, data_reference='steps.1.produce')
+    step_2.add_hyperparameter(name='columns', argument_type=ArgumentType.VALUE, data=[0])
+    step_2.add_output('produce')
+    pipeline_description.add_step(step_2)
+
+    pipeline_description.add_output(name='Result', data_reference='steps.2.produce')
 
     return pipeline_description
+
